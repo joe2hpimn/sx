a0458d23ca386e0a20884761373b8bc22ccbe01c
Török Edwin <edwin@skylable.com>
2014-04-07 15:26:09 +0300

    only mkdir when recursive

--- libsx/src/fileops.c
+++ libsx/src/fileops.c
@@ -3766,7 +3766,8 @@ static sxi_job_t* remote_copy_ev(sxc_file_t *pattern, sxc_file_t *source, sxc_fi
         return NULL;
     if(!is_remote(dest)) {
         const char *msg;
-        mkdir_parents(dest->sx, dest->path);
+        if (recursive)
+            mkdir_parents(dest->sx, dest->path);
         int ret = remote_to_local(source, dest, rs);
         msg = sxc_geterrnum(source->sx) == SXE_NOERROR ? "OK" : sxc_geterrmsg(source->sx);
         sxi_info(source->sx, "%s: %s", dest->path, msg);
