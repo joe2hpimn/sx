e545cff09eb5efaf9564ec15eea27606550ac1cf
Török Edwin <edwin@skylable.com>
2014-09-23 10:26:13 +0300

    don't warn on SQLITE_LOCKED in DB checkpoint

--- server/src/common/sxdbi.c
+++ server/src/common/sxdbi.c
@@ -104,8 +104,8 @@ static void qcheckpoint_run(sxi_db_t *db, int kind)
     gettimeofday(&tv0, NULL);
     rc = sqlite3_wal_checkpoint_v2(db->handle, NULL, kind, &log, &ckpt);
     gettimeofday(&tv1, NULL);
-    if (rc != SQLITE_OK && rc != SQLITE_BUSY) {
-        WARN("Failed to checkpoint GC db: %s", sqlite3_errmsg(db->handle));
+    if (rc != SQLITE_OK && rc != SQLITE_BUSY && rc != SQLITE_LOCKED) {
+        WARN("Failed to checkpoint db '%s': %s", sqlite3_db_filename(db->handle, "main"), sqlite3_errmsg(db->handle));
     } else if (ckpt > 0) {
         DEBUG("WAL %s: %d frames, %d checkpointed: %s in %.1fs", sqlite3_db_filename(db->handle, "main"), log, ckpt, sqlite3_errmsg(db->handle),
              timediff(&tv0, &tv1));
