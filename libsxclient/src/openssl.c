ca51bc44809331e15d053ba7d6cfcd5ca12cad81
Török Edwin <edwin@skylable.com>
2015-10-13 16:18:13 +0300

    compatibility with curl 7.45.0 (bb#1541)

--- libsxclient/src/openssl.c
+++ libsxclient/src/openssl.c
@@ -98,6 +98,8 @@ int sxi_sslctxfun(sxc_client_t *sx, curlev_t *ev, const struct curl_tlssessionin
         return -1;
     }
     SSL_CTX *ctx = (SSL_CTX*)info->internals;
+    if (!ctx)
+        return -EAGAIN;
     SSL_CTX_set_cert_verify_callback(ctx, ssl_verify_hostname, ev);
     return 0;
 }
