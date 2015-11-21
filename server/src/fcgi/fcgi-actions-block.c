joe2hpimn/sx

commit c5f28128a3f1388e5e415614935ec725653395de

introducing jparse, a loose JSON parser

server/src/fcgi/fcgi-actions-block.c
@@ -36,6 +36,7 @@
 #include "blob.h"
 #include "fcgi-actions-block.h"
 #include "job_common.h"
+#include "libsxclient/src/jparse.h"
 
 void fcgi_send_blocks(void) {
     unsigned int blocksize;
@@ -567,144 +568,87 @@ void fcgi_save_blocks(void) {
     CGI_PUTS("\r\n");
 }
 
+/* {"hash1":["uuid1", "uuid2""], "hash2":["uuid3", "uuid1"], ...} */
 struct pushblox_ctx {
     sx_blob_t *stash;
     rc_ty error;
-    enum pushblox_state { CB_PB_START, CB_PB_HASH, CB_PB_TARGETS, CB_PB_TARGET, CB_PB_COMPLETE } state;
 };
 
-static int cb_pushblox_start_map(void *ctx) {
+void cb_pushblox_target(jparse_t *J, void *ctx, const char *string, unsigned int length) {
+    jploc_t *lkey = sxi_jparse_whereami(J), *lhost = sxi_jpath_down(lkey);
     struct pushblox_ctx *c = ctx;
-    if(c->state == CB_PB_START)
-	c->state = CB_PB_HASH;
-    else
-	return 0;
-    return 1;
-}
 
-static int cb_pushblox_end_map(void *ctx) {
-    struct pushblox_ctx *c = ctx;
-    if(c->state != CB_PB_HASH)
-	return 0;
-
-    if(sx_blob_add_blob(c->stash, "", 1)) {
-	c->error = ENOMEM;
-	return 0;
+    if(!sxi_jpath_arraypos(lhost)) { /* First target of a block */
+	const char *key = sxi_jpath_mapkey(lkey);
+	sx_hash_t block;
+	if(strlen(key) != sizeof(block) * 2 ||
+	   hex2bin(key, sizeof(block) * 2, (uint8_t *)&block, sizeof(block))) {
+	    c->error = EINVAL;
+	    sxi_jparse_cancel(J, "Invalid block name '%s'", key);
+	    return;
+	}
+	if(sx_blob_add_blob(c->stash, &block, sizeof(block))) {
+	    c->error = ENOMEM;
+	    sxi_jparse_cancel(J, "Out of memory");
+	    return;
+	}
     }
 
-    c->state = CB_PB_COMPLETE;
-    return 1;
-}
+    if(length == UUID_STRING_SIZE) {
+	char uuidstr[UUID_STRING_SIZE+1];
+	sx_uuid_t uuid;
 
-static int cb_pushblox_map_key(void *ctx, const unsigned char *s, size_t l) {
-    struct pushblox_ctx *c = ctx;
-    sx_hash_t block;
-
-    if(c->state != CB_PB_HASH)
-	return 0;
-
-    c->state = CB_PB_TARGETS;
-    if(hex2bin(s, l, (uint8_t *)&block, sizeof(block)))
-	return 0;
-
-    if(sx_blob_add_blob(c->stash, &block, sizeof(block))) {
-	c->error = ENOMEM;
-	return 0;
+	memcpy(uuidstr, string, UUID_STRING_SIZE);
+	uuidstr[UUID_STRING_SIZE] = '\0';
+	if(!uuid_from_string(&uuid, uuidstr)) {
+	    if(sx_blob_add_blob(c->stash, uuid.binary, sizeof(uuid.binary))) {
+		c->error = ENOMEM;
+		sxi_jparse_cancel(J, "Out of memory");
+	    }
+	    return; /* Target added */
+	}
     }
-
-    return 1;
+    sxi_jparse_cancel(J, "Invalid target uuid '%.*s'", length, string);
+    c->error = EINVAL;
 }
-
-static int cb_pushblox_start_array(void *ctx) {
-    struct pushblox_ctx *c = ctx;
-    if(c->state == CB_PB_TARGETS)
-	c->state = CB_PB_TARGET;
-    else
-	return 0;
-    return 1;
-}
-
-static int cb_pushblox_end_array(void *ctx) {
-    struct pushblox_ctx *c = ctx;
-    if(c->state == CB_PB_TARGET)
-	c->state = CB_PB_HASH;
-    else
-	return 0;
-    return 1;
-}
-
-static int cb_pushblox_string(void *ctx, const unsigned char *s, size_t l) {
-    struct pushblox_ctx *c = ctx;
-    char uuidstr[UUID_STRING_SIZE+1];
-    sx_uuid_t uuid;
-
-    if(c->state != CB_PB_TARGET)
-	return 0;
-
-    if(l != UUID_STRING_SIZE)
-	return 0;
-
-    memcpy(uuidstr, s, UUID_STRING_SIZE);
-    uuidstr[UUID_STRING_SIZE] = '\0';
-
-    if(uuid_from_string(&uuid, uuidstr))
-	return 0;
-
-    if(sx_blob_add_blob(c->stash, uuid.binary, sizeof(uuid.binary))) {
-	c->error = ENOMEM;
-	return 0;
-    }
-
-    return 1;
-}
-
-static const yajl_callbacks pushblx_parser = {
-    cb_fail_null,
-    cb_fail_boolean,
-    NULL,
-    NULL,
-    cb_fail_number,
-    cb_pushblox_string,
-    cb_pushblox_start_map,
-    cb_pushblox_map_key,
-    cb_pushblox_end_map,
-    cb_pushblox_start_array,
-    cb_pushblox_end_array
-};
 
 
 void fcgi_push_blocks(void) {
+    const struct jparse_actions acts = {
+	JPACTS_STRING(JPACT(cb_pushblox_target, JPANYKEY, JPANYITM))
+    };
     struct pushblox_ctx yctx;
     unsigned int blocksize;
     const char *eop;
+    jparse_t *J;
+    int len;
 
     blocksize = strtol(path, (char **)&eop, 10);
     if(*eop || sx_hashfs_check_blocksize(blocksize) != OK)
 	quit_errmsg(404, "Invalid blocksize");
 
+    J = sxi_jparse_create(&acts, &yctx, 0);
+    if(!J)
+	quit_errmsg(503, "Cannot create JSON parser");
+
     yctx.stash = sx_blob_new();
-    if(!yctx.stash)
-	quit_errmsg(500, "Cannot allocate temporary storage");
-    yctx.state = CB_PB_START;
-    yctx.error = EINVAL;
-
-    yajl_handle yh = yajl_alloc(&pushblx_parser, NULL, &yctx);
-    if(!yh) {
-	sx_blob_free(yctx.stash);
-	quit_errmsg(500, "Cannot allocate json parser");
+    if(!yctx.stash) {
+	sxi_jparse_destroy(J);
+	quit_errmsg(503, "Cannot allocate temporary storage");
     }
 
-    int len;
     while((len = get_body_chunk(hashbuf, sizeof(hashbuf))) > 0)
-	if(yajl_parse(yh, hashbuf, len) != yajl_status_ok) break;
+	if(sxi_jparse_digest(J, hashbuf, len))
+	    break;
 
-    if(len || yajl_complete_parse(yh) != yajl_status_ok || yctx.state != CB_PB_COMPLETE) {
-	yajl_free(yh);
+    if(len || sxi_jparse_done(J)) {
+	send_error(rc2http(yctx.error), sxi_jparse_geterr(J));
 	sx_blob_free(yctx.stash);
-	quit_errmsg(rc2http(yctx.error), (yctx.error == ENOMEM) ? "Out of memory processing the request" : "Invalid request content");
+	sxi_jparse_destroy(J);
+	return;
     }
-    yajl_free(yh);
 
+    sxi_jparse_destroy(J);
     auth_complete();
     if(!is_authed()) {
 	sx_blob_free(yctx.stash);

libsxclient/Makefile.am
@@ -32,6 +32,8 @@ src_libsxclient_la_SOURCES = \
 	src/hostlist.h \
 	src/clustcfg.c \
 	src/clustcfg.h \
+	src/jparse.c \
+	src/jparse.h \
 	src/yajlwrap.c \
 	src/yajlwrap.h \
 	src/misc.c \

libsxclient/Makefile.in
@@ -157,9 +157,10 @@ am__src_libsxclient_la_SOURCES_DIST = include/sx.h include/default.h \
 	include/gnuc.h include/types.h include/version.h \
 	src/curlevents.c src/curlevents.h src/curlevents-common.h \
 	src/cluster.c src/cluster.h src/hostlist.c src/hostlist.h \
-	src/clustcfg.c src/clustcfg.h src/yajlwrap.c src/yajlwrap.h \
-	src/misc.c src/misc.h src/fileops.c src/fileops.h src/volops.c \
-	src/volops.h src/jobpoll.c src/jobpoll.h src/libsxclient.c \
+	src/clustcfg.c src/clustcfg.h src/jparse.c src/jparse.h \
+	src/yajlwrap.c src/yajlwrap.h src/misc.c src/misc.h \
+	src/fileops.c src/fileops.h src/volops.c src/volops.h \
+	src/jobpoll.c src/jobpoll.h src/libsxclient.c \
 	src/libsxclient-int.h src/filter.c src/filter.h src/sxlog.h \
 	src/sxlog.c src/sxproto.h src/sxproto.c src/sxreport.h \
 	src/sxreport.c src/version.c src/vcrypto.h src/vcryptocurl.h \
@@ -174,6 +175,7 @@ am_src_libsxclient_la_OBJECTS = src/src_libsxclient_la-curlevents.lo \
 	src/src_libsxclient_la-cluster.lo \
 	src/src_libsxclient_la-hostlist.lo \
 	src/src_libsxclient_la-clustcfg.lo \
+	src/src_libsxclient_la-jparse.lo \
 	src/src_libsxclient_la-yajlwrap.lo \
 	src/src_libsxclient_la-misc.lo \
 	src/src_libsxclient_la-fileops.lo \
@@ -468,6 +470,7 @@ pdfdir = @pdfdir@
 prefix = @prefix@
 program_transform_name = @program_transform_name@
 psdir = @psdir@
+runstatedir = @runstatedir@
 sbindir = @sbindir@
 sharedstatedir = @sharedstatedir@
 srcdir = @srcdir@
@@ -491,9 +494,10 @@ src_libsxclient_la_SOURCES = include/sx.h include/default.h \
 	include/gnuc.h include/types.h include/version.h \
 	src/curlevents.c src/curlevents.h src/curlevents-common.h \
 	src/cluster.c src/cluster.h src/hostlist.c src/hostlist.h \
-	src/clustcfg.c src/clustcfg.h src/yajlwrap.c src/yajlwrap.h \
-	src/misc.c src/misc.h src/fileops.c src/fileops.h src/volops.c \
-	src/volops.h src/jobpoll.c src/jobpoll.h src/libsxclient.c \
+	src/clustcfg.c src/clustcfg.h src/jparse.c src/jparse.h \
+	src/yajlwrap.c src/yajlwrap.h src/misc.c src/misc.h \
+	src/fileops.c src/fileops.h src/volops.c src/volops.h \
+	src/jobpoll.c src/jobpoll.h src/libsxclient.c \
 	src/libsxclient-int.h src/filter.c src/filter.h src/sxlog.h \
 	src/sxlog.c src/sxproto.h src/sxproto.c src/sxreport.h \
 	src/sxreport.c src/version.c src/vcrypto.h src/vcryptocurl.h \
@@ -617,6 +621,8 @@ src/src_libsxclient_la-hostlist.lo: src/$(am__dirstamp) \
 	src/$(DEPDIR)/$(am__dirstamp)
 src/src_libsxclient_la-clustcfg.lo: src/$(am__dirstamp) \
 	src/$(DEPDIR)/$(am__dirstamp)
+src/src_libsxclient_la-jparse.lo: src/$(am__dirstamp) \
+	src/$(DEPDIR)/$(am__dirstamp)
 src/src_libsxclient_la-yajlwrap.lo: src/$(am__dirstamp) \
 	src/$(DEPDIR)/$(am__dirstamp)
 src/src_libsxclient_la-misc.lo: src/$(am__dirstamp) \
@@ -670,6 +676,7 @@ distclean-compile:
 @AMDEP_TRUE@@am__include@ @am__quote@src/$(DEPDIR)/src_libsxclient_la-filter.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@src/$(DEPDIR)/src_libsxclient_la-hostlist.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@src/$(DEPDIR)/src_libsxclient_la-jobpoll.Plo@am__quote@
+@AMDEP_TRUE@@am__include@ @am__quote@src/$(DEPDIR)/src_libsxclient_la-jparse.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@src/$(DEPDIR)/src_libsxclient_la-libsxclient.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@src/$(DEPDIR)/src_libsxclient_la-misc.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@src/$(DEPDIR)/src_libsxclient_la-nftw.Plo@am__quote@
@@ -733,6 +740,13 @@ src/src_libsxclient_la-clustcfg.lo: src/clustcfg.c
 @AMDEP_TRUE@@am__fastdepCC_FALSE@	$(AM_V_CC)source='src/clustcfg.c' object='src/src_libsxclient_la-clustcfg.lo' libtool=yes @AMDEPBACKSLASH@
 @AMDEP_TRUE@@am__fastdepCC_FALSE@	DEPDIR=$(DEPDIR) $(CCDEPMODE) $(depcomp) @AMDEPBACKSLASH@
 @am__fastdepCC_FALSE@	$(AM_V_CC@am__nodep@)$(LIBTOOL) $(AM_V_lt) --tag=CC $(AM_LIBTOOLFLAGS) $(LIBTOOLFLAGS) --mode=compile $(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(src_libsxclient_la_CPPFLAGS) $(CPPFLAGS) $(AM_CFLAGS) $(CFLAGS) -c -o src/src_libsxclient_la-clustcfg.lo `test -f 'src/clustcfg.c' || echo '$(srcdir)/'`src/clustcfg.c
+
+src/src_libsxclient_la-jparse.lo: src/jparse.c
+@am__fastdepCC_TRUE@	$(AM_V_CC)$(LIBTOOL) $(AM_V_lt) --tag=CC $(AM_LIBTOOLFLAGS) $(LIBTOOLFLAGS) --mode=compile $(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(src_libsxclient_la_CPPFLAGS) $(CPPFLAGS) $(AM_CFLAGS) $(CFLAGS) -MT src/src_libsxclient_la-jparse.lo -MD -MP -MF src/$(DEPDIR)/src_libsxclient_la-jparse.Tpo -c -o src/src_libsxclient_la-jparse.lo `test -f 'src/jparse.c' || echo '$(srcdir)/'`src/jparse.c
+@am__fastdepCC_TRUE@	$(AM_V_at)$(am__mv) src/$(DEPDIR)/src_libsxclient_la-jparse.Tpo src/$(DEPDIR)/src_libsxclient_la-jparse.Plo
+@AMDEP_TRUE@@am__fastdepCC_FALSE@	$(AM_V_CC)source='src/jparse.c' object='src/src_libsxclient_la-jparse.lo' libtool=yes @AMDEPBACKSLASH@
+@AMDEP_TRUE@@am__fastdepCC_FALSE@	DEPDIR=$(DEPDIR) $(CCDEPMODE) $(depcomp) @AMDEPBACKSLASH@
+@am__fastdepCC_FALSE@	$(AM_V_CC@am__nodep@)$(LIBTOOL) $(AM_V_lt) --tag=CC $(AM_LIBTOOLFLAGS) $(LIBTOOLFLAGS) --mode=compile $(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(src_libsxclient_la_CPPFLAGS) $(CPPFLAGS) $(AM_CFLAGS) $(CFLAGS) -c -o src/src_libsxclient_la-jparse.lo `test -f 'src/jparse.c' || echo '$(srcdir)/'`src/jparse.c
 
 src/src_libsxclient_la-yajlwrap.lo: src/yajlwrap.c
 @am__fastdepCC_TRUE@	$(AM_V_CC)$(LIBTOOL) $(AM_V_lt) --tag=CC $(AM_LIBTOOLFLAGS) $(LIBTOOLFLAGS) --mode=compile $(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(src_libsxclient_la_CPPFLAGS) $(CPPFLAGS) $(AM_CFLAGS) $(CFLAGS) -MT src/src_libsxclient_la-yajlwrap.lo -MD -MP -MF src/$(DEPDIR)/src_libsxclient_la-yajlwrap.Tpo -c -o src/src_libsxclient_la-yajlwrap.lo `test -f 'src/yajlwrap.c' || echo '$(srcdir)/'`src/yajlwrap.c

libsxclient/src/jparse.c
@@ -1,0 +1,424 @@
+/*
+ *  Copyright (C) 2015 Skylable Ltd. <info-copyright@skylable.com>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#include "default.h"
+
+#include <yajl/yajl_parse.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <errno.h>
+
+#include "jparse.h"
+
+
+struct container {
+    struct container *up, *down;
+    const char *key;
+    int pos;
+};
+
+struct jparse {
+    struct container *first, *last;
+    void *ctx; /* Opaque pointer */
+    const struct jparse_actions *act;
+    yajl_handle yh;
+    int newlvl, cancelled, parseerr;
+    char errbuf[256];
+};
+
+
+static int jparse_cmp(struct jparse *J, const struct jref **ref) {
+    struct container *a = J->first;
+    unsigned int i;
+    for(i=0; ; i++, a=a->down) {
+	const struct jref *b = ref[i];
+	int cmp;
+
+	if(!a) /* json ends */
+	    return b ? 1 : 0;
+	if(!b)/* ref ends */
+	    return -1;
+
+	if(a->key) { /* json is a map */
+	    if(b->pos == JPWLDKEY) /* ref is a map wildcard */
+		continue;
+	    if(!b->key) /* ref is an array */
+		return -1;
+	    /* Both are maps, compare keys */
+	    cmp = strcmp(a->key, b->key);
+	} else { /* json is an array */
+	    if(b->pos == JPWLDITM) /* ref is an array wildcard */
+		continue;
+	    if(b->key)
+		return 1;
+	    /* Both are arrays, compare position */
+	    cmp = a->pos - b->pos;
+	}
+	if(cmp)
+	    return cmp;
+    }
+    return 0;
+}
+
+#define NONULLJ do {						\
+	if(!J)							\
+	    return 0;						\
+    } while(0)
+
+#define NONULLARGS do {						\
+	if(!J || !s)						\
+	    return 0;						\
+    } while(0)
+
+static __inline__ void bumpar(struct jparse *J) {
+    /* If in array bump position */
+    if(J && J->last && !J->last->key)
+	J->last->pos++;
+}
+
+#define FOREACH_MATCH(TYPE, ...)					\
+    do{if(J->act && J->act->TYPE) {					\
+	unsigned int i = 0;						\
+	while(J->act->TYPE[i].ref) {					\
+	    if(!jparse_cmp(J, J->act->TYPE[i].ref) && J->act->TYPE[i].cb) \
+		J->act->TYPE[i].cb(J, __VA_ARGS__);			\
+	    if(J->cancelled)						\
+		return 0;						\
+	    i++;							\
+	}								\
+    }} while(0)
+
+static int jpnull(void *ctx) {
+    struct jparse *J = (struct jparse *)ctx;
+
+    NONULLJ;
+
+    FOREACH_MATCH(actions_null, J->ctx);
+    bumpar(J);
+    return 1;
+}
+
+static int jpboolean(void *ctx, int boolean) {
+    struct jparse *J = (struct jparse *)ctx;
+
+    NONULLJ;
+
+    FOREACH_MATCH(actions_bool, J->ctx, boolean);
+    bumpar(J);
+    return 1;
+}
+
+static int jpnumber(void *ctx, const char *s, size_t l) {
+    struct jparse *J = (struct jparse *)ctx;
+    char numstr[32];
+
+    NONULLARGS;
+
+    if(J->act && l < sizeof(numstr)) {
+	char *eon;
+	do {
+	    double num;
+	    if(!J->act->actions_double)
+		break;
+	    memcpy(numstr, s, l);
+	    numstr[l] = '\0';
+	    errno = 0;
+	    num = strtod(numstr, &eon);
+	    if(errno || eon == numstr || *eon)
+		break;
+	    FOREACH_MATCH(actions_double, J->ctx, num);
+	} while(0);
+	do {
+	    int64_t num;
+	    if(!J->act->actions_int32 && !J->act->actions_int64)
+		break;
+	    if(!J->act->actions_double) {
+		memcpy(numstr, s, l);
+		numstr[l] = '\0';
+	    }
+	    errno = 0;
+	    num = strtoll(numstr, &eon, 10);
+	    if(errno || eon == numstr || *eon)
+		break;
+	    FOREACH_MATCH(actions_int64, J->ctx, num);
+	    if(num > 0x7fffffff || num < -0x7fffffff)
+		break;
+	    FOREACH_MATCH(actions_int32, J->ctx, num);
+	} while(0);
+    }
+
+    bumpar(J);
+    return 1;
+}
+
+static int jpstring(void * ctx, const unsigned char *s, size_t l) {
+    struct jparse *J = (struct jparse *)ctx;
+
+    NONULLARGS;
+
+    FOREACH_MATCH(actions_string, J->ctx, (const char *)s, (unsigned int)l);
+    bumpar(J);
+    return 1;
+}
+
+
+static int jpstart_map(void *ctx) {
+    struct jparse *J = (struct jparse *)ctx;
+
+    NONULLJ;
+    if(J->newlvl) {
+	/* Not reached */
+	fprintf(stderr, "start_map: invalid input");
+	return 0;
+    }
+
+    FOREACH_MATCH(actions_mapstart, J->ctx);
+    J->newlvl = 1;
+    return 1;
+}
+
+static int jpmap_key(void *ctx, const unsigned char *s, size_t l) {
+    struct jparse *J = (struct jparse *)ctx;
+    struct container *c, *last;
+    char *buf;
+
+    NONULLARGS;
+    c = malloc(sizeof(*c) + l + 1);
+    if(!c) {
+	fprintf(stderr, "map_key: Out of memory");
+	return 0;
+    }
+    buf = (char *)(c+1);
+    memcpy(buf, s, l);
+    buf[l] = '\0';
+    c->key = buf;
+    c->pos = 0;
+    c->down = NULL;
+
+    last = J->last;
+    J->last = c;
+    if(J->newlvl) {
+	/* Append */
+	J->newlvl = 0;
+	if(last)
+	    last->down = c;
+	else
+	    J->first = c;
+	c->up = last;
+    } else {
+	/* Replace */
+	struct container *parent = last->up;
+	if(parent)
+	    parent->down = c;
+	else
+	    J->first = c;
+	c->up = parent;
+	free(last);
+    }
+
+    return 1;
+}
+
+static int jpend_obj(void *ctx, int ismap) {
+    struct jparse *J = (struct jparse *)ctx;
+    struct container *last;
+
+    NONULLJ;
+    last = J->last;
+    if(last) {
+	J->last = last->up;
+	if(J->last)
+	    J->last->down = NULL;
+	else
+	    J->first = NULL;
+	free(last);
+    } else {
+	J->first = NULL;
+	J->last = NULL;
+    }
+	
+    if(ismap)
+	FOREACH_MATCH(actions_mapend, J->ctx);
+    else
+	FOREACH_MATCH(actions_arrend, J->ctx);
+
+    bumpar(J);
+    return 1;
+}
+
+
+static int jpend_map(void *ctx) {
+    return jpend_obj(ctx, 1);
+}
+
+static int jpend_array(void *ctx) {
+    return jpend_obj(ctx, 0);
+}
+
+static int jpstart_array(void *ctx) {
+    struct jparse *J = (struct jparse *)ctx;
+    struct container *c, *last;
+
+    NONULLJ;
+
+    FOREACH_MATCH(actions_arrstart, J->ctx);
+
+    c = malloc(sizeof(*c));
+    if(!c) {
+	fprintf(stderr, "start_array: Out of memory");
+	return 0;
+    }
+    c->key = NULL;
+    c->pos = 0;
+    c->down = NULL;
+
+    last = J->last;
+    J->last = c;
+    if(last)
+	last->down = c;
+    else
+	J->first = c;
+    c->up = last;
+    return 1;
+}
+
+static const yajl_callbacks callbacks = {
+    jpnull,
+    jpboolean,
+    NULL,
+    NULL,
+    jpnumber,
+    jpstring,
+    jpstart_map,
+    jpmap_key,
+    jpend_map,
+    jpstart_array,
+    jpend_array
+};
+
+struct jparse *sxi_jparse_create(const struct jparse_actions *actions, void *ctx, int parseerr) {
+    struct jparse *J;
+    J = calloc(1, sizeof(*J));
+    if(!J)
+	return NULL;
+
+    J->yh = yajl_alloc(&callbacks, NULL, J);
+    if(!J->yh) {
+	free(J);
+	return NULL;
+    }
+
+    J->act = actions;
+    J->ctx = ctx;
+    J->parseerr = parseerr;
+    return J;
+}
+
+void sxi_jparse_destroy(struct jparse *J) {
+    if(J) {
+	struct container *c1 = J->last, *c2;
+	while(c1) {
+	    c2 = c1->up;
+	    free(c1);
+	    c1 = c2;
+	}
+	if(J->yh)
+	    yajl_free(J->yh);
+	free(J);
+    }
+}
+
+static int yajl_res(struct jparse *J, yajl_status res) {
+    unsigned char *yreason = NULL;
+
+    switch(res) {
+    case yajl_status_ok:
+	return 0;
+    case yajl_status_client_canceled:
+	if(!J->errbuf[0]) /* Not reached */
+	    snprintf(J->errbuf, sizeof(J->errbuf), "Operation cancelled by callback");
+	return 1;
+    case yajl_status_error:
+	yreason = yajl_get_error(J->yh, 0, (const unsigned char *)"", 0);
+    default:
+	if(yreason) {
+	    snprintf(J->errbuf, sizeof(J->errbuf), "JSON parser failed: %s", yreason);
+	    yajl_free_error(J->yh, yreason);
+	} else
+	    snprintf(J->errbuf, sizeof(J->errbuf), "JSON parser failed for unspecified reasons");
+	return 1;
+    }
+}
+
+int sxi_jparse_digest(struct jparse *J, const void *data, unsigned int datalen) {
+    return yajl_res(J, yajl_parse(J->yh, data, datalen));
+}
+
+int sxi_jparse_done(struct jparse *J) {
+    return yajl_res(J, yajl_complete_parse(J->yh));
+}
+
+const char *sxi_jparse_geterr(struct jparse *J) {
+    if(J->errbuf[0])
+	return J->errbuf;
+    else
+	return "No error";
+}
+
+void sxi_jparse_cancel(struct jparse *J, const char *reason_fmt, ...) {
+    va_list ap;
+    if(!J->cancelled) {
+	va_start(ap, reason_fmt);
+	J->cancelled = 1;
+	vsnprintf(J->errbuf, sizeof(J->errbuf), reason_fmt, ap);
+	va_end(ap);
+    }
+}
+
+const struct container *sxi_jparse_whereami(struct jparse *J) {
+    return J ? J->first : NULL;
+}
+
+int sxi_jpath_ismap(const struct container *jploc) {
+    return jploc && jploc->key != NULL;
+}
+
+const char *sxi_jpath_mapkey(const struct container *jploc) {
+    return sxi_jpath_ismap(jploc) ? jploc->key : NULL;
+}
+
+int sxi_jpath_isarray(const struct container *jploc) {
+    return jploc && jploc->key == NULL;
+}
+
+int sxi_jpath_arraypos(const struct container *jploc) {
+    return sxi_jpath_isarray(jploc) ? jploc->pos : -1;
+}
+
+const struct container *sxi_jpath_up(const struct container *jploc) {
+    return jploc ? jploc->up : NULL;
+}
+
+const struct container *sxi_jpath_down(const struct container *jploc) {
+    return jploc ? jploc->down : NULL;
+}
+

libsxclient/src/jparse.h
@@ -1,0 +1,134 @@
+/*
+ *  Copyright (C) 2015 Skylable Ltd. <info-copyright@skylable.com>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#ifndef JPARSE_H
+#define JPARSE_H
+
+#include <stdint.h>
+
+/* The function protoes are near the end of this file */
+
+struct jref {
+    const char *key;
+    int pos;
+};
+
+#define JPKEY(k) (&(const struct jref){ k, 0 })
+#define JPARR(n) (&(const struct jref){ NULL, n })
+#define JPWLDKEY -2
+#define JPWLDITM -1
+#define JPANYKEY (&(const struct jref){ NULL, JPWLDKEY })
+#define JPANYITM (&(const struct jref){ NULL, JPWLDITM })
+
+#define JPREF(...) (const struct jref *[]){ __VA_ARGS__, NULL }
+
+
+typedef struct jparse jparse_t;
+
+typedef void (*jpmatch_nullfn)(jparse_t *J, void *ctx);
+struct jpmatch_null {
+    jpmatch_nullfn cb;
+    const struct jref **ref;
+};
+#define JPACTS_NULL(...) .actions_null = (const struct jpmatch_null []) { __VA_ARGS__, { NULL, NULL } }
+
+typedef void (*jpmatch_boolfn)(jparse_t *J, void *ctx, int val);
+struct jpmatch_bool {
+    jpmatch_boolfn cb;
+    const struct jref **ref;
+};
+#define JPACTS_BOOL(...) .actions_bool = (const struct jpmatch_bool []) { __VA_ARGS__, { NULL, NULL } }
+
+typedef void (*jpmatch_int32fn)(jparse_t *J, void *ctx, int32_t num);
+struct jpmatch_int32 {
+    jpmatch_int32fn cb;
+    const struct jref **ref;
+};
+#define JPACTS_INT32(...) .actions_int32 = (const struct jpmatch_int32 []) { __VA_ARGS__, { NULL, NULL } }
+
+typedef void (*jpmatch_int64fn)(jparse_t *J, void *ctx, int64_t num);
+struct jpmatch_int64 {
+    jpmatch_int64fn cb;
+    const struct jref **ref;
+};
+#define JPACTS_INT64(...) .actions_int64 = (const struct jpmatch_int64 []) { __VA_ARGS__, { NULL, NULL } }
+
+typedef void (*jpmatch_double)(jparse_t *J, void *ctx, double num);
+struct jpmatch_double {
+    jpmatch_double cb;
+    const struct jref **ref;
+};
+#define JPACTS_DOUBLE(...) .actions_double = (const struct jpmatch_double []) { __VA_ARGS__, { NULL, NULL } }
+
+typedef void (*jpmatch_string)(jparse_t *J, void *ctx, const char *string, unsigned int length);
+struct jpmatch_string {
+    jpmatch_string cb;
+    const struct jref **ref;
+};
+#define JPACTS_STRING(...) .actions_string = (const struct jpmatch_string []) { __VA_ARGS__, { NULL, NULL } }
+
+typedef void (*jpmatch_container)(jparse_t *J, void *ctx);
+struct jpmatch_container {
+    jpmatch_container cb;
+    const struct jref **ref;
+};
+#define JPACTS_ARRAY_BEGIN(...) .actions_arrstart = (const struct jpmatch_container []) { __VA_ARGS__, { NULL, NULL } }
+#define JPACTS_ARRAY_END(...) .actions_arrend = (const struct jpmatch_container []) { __VA_ARGS__, { NULL, NULL } }
+#define JPACTS_MAP_BEGIN(...) .actions_mapstart = (const struct jpmatch_container []) { __VA_ARGS__, { NULL, NULL } }
+#define JPACTS_MAP_END(...) .actions_mapend = (const struct jpmatch_container []) { __VA_ARGS__, { NULL, NULL } }
+
+
+struct jparse_actions {
+    const struct jpmatch_null *actions_null;
+    const struct jpmatch_bool *actions_bool;
+    const struct jpmatch_int32 *actions_int32;
+    const struct jpmatch_int64 *actions_int64;
+    const struct jpmatch_double *actions_double;
+    const struct jpmatch_string *actions_string;
+    /* Convenience virtual leaves, not real JSON values */
+    const struct jpmatch_container *actions_arrstart;
+    const struct jpmatch_container *actions_arrend;
+    const struct jpmatch_container *actions_mapstart;
+    const struct jpmatch_container *actions_mapend;
+};
+#define JPACT(callback_fn, ...) { callback_fn, JPREF(__VA_ARGS__) }
+
+/* Parser creation and destruction */
+jparse_t *sxi_jparse_create(const struct jparse_actions *actions, void *ctx, int parseerr);
+void sxi_jparse_destroy(jparse_t *J);
+
+/* JSON data digestion */
+int sxi_jparse_digest(jparse_t *J, const void *data, unsigned int datalen);
+int sxi_jparse_done(jparse_t *J);
+const char *sxi_jparse_geterr(jparse_t *J);
+
+/* In-callback functions */
+void sxi_jparse_cancel(jparse_t *J, const char *reason_fmt, ...);
+typedef const struct container jploc_t;
+jploc_t *sxi_jparse_whereami(jparse_t *J);
+
+/* Path examination and navigation functions */
+int sxi_jpath_ismap(jploc_t *jploc);
+const char *sxi_jpath_mapkey(jploc_t *jploc);
+int sxi_jpath_isarray(jploc_t *jploc);
+int sxi_jpath_arraypos(jploc_t *jploc);
+jploc_t *sxi_jpath_up(jploc_t *jploc);
+jploc_t *sxi_jpath_down(jploc_t *jploc);
+
+#endif

server/src/common/hashfs.c
@@ -2742,7 +2742,7 @@ rc_ty sx_hashfs_check_volume_meta(const char *key, const void *value, unsigned i
         return s;
     /* Check if the volume meta does not contain reserved prefix */
     if(check_prefix && strlen(key) >= lenof(SX_CUSTOM_META_PREFIX) && !strncmp(SX_CUSTOM_META_PREFIX, key, lenof(SX_CUSTOM_META_PREFIX))) {
-        INFO("Volume meta key cannot contain reserved prefix %s", SX_CUSTOM_META_PREFIX);
+        msg_set_reason("Volume meta key cannot contain reserved prefix %s", SX_CUSTOM_META_PREFIX);
         return EINVAL;
     }
 
@@ -8914,8 +8914,10 @@ rc_ty sx_hashfs_putfile_putmeta(sx_hashfs_t *h, const char *key, const void *val
     if(!h)
 	return FAIL_EINTERNAL;
 
-    if(h->nmeta >= SXLIMIT_META_MAX_ITEMS)
+    if(h->nmeta >= SXLIMIT_META_MAX_ITEMS) {
+	msg_set_reason("Too many metadata entries");
 	return EOVERFLOW;
+    }
 
     if(!value) {
 	/* Delete key: check key (and bogus value) */

server/src/common/job_common.h
@@ -28,9 +28,9 @@
 #ifndef JOB_COMMON_H
 #define JOB_COMMON_H
 
-#include <yajl/yajl_parse.h>
 #include "blob.h"
 #include "../../libsxclient/src/sxproto.h"
+#include "../../libsxclient/src/jparse.h"
 
 typedef int64_t job_t;
 
@@ -91,7 +91,7 @@ typedef enum {
 
 struct _sx_hashfs_t; /* fwd */
 typedef struct {
-    const yajl_callbacks *parser;
+    const struct jparse_actions *jpacts;
     int job_type;
     rc_ty (*parse_complete)(void *ctx);
     const char* (*get_lock)(sx_blob_t *blob);

commit 454ec7ecacc5d812829c454df84ecffe440d4069

Properly terminate propagation list Regression introduced in c5f28128a3f1388e5e415614935ec725653395de

server/src/fcgi/fcgi-actions-block.c
@@ -568,7 +568,7 @@ void fcgi_save_blocks(void) {
     CGI_PUTS("\r\n");
 }
 
-/* {"hash1":["uuid1", "uuid2""], "hash2":["uuid3", "uuid1"], ...} */
+/* {"hash1":["uuid1", "uuid2"], "hash2":["uuid3", "uuid1"], ...} */
 struct pushblox_ctx {
     sx_blob_t *stash;
     rc_ty error;
@@ -656,6 +656,10 @@ void fcgi_push_blocks(void) {
 	return;
     }
 
+    if(sx_blob_add_blob(yctx.stash, "", 1)) {
+	sx_blob_free(yctx.stash);
+	quit_errmsg(503, "Out of memory building propagation list");
+    }
     sx_blob_loadpos(yctx.stash); /* Reset blob */
 
     sx_hash_t block;