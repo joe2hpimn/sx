joe2hpimn/sx

commit 0113b9005563e922e1ea6ac1987bea8a8bfd8fac

Make user list query cachable (bb#1990)

server/src/fcgi/fcgi-actions-user.c
@@ -1275,7 +1275,9 @@ void fcgi_list_users(void) {
     int first = 1;
     const char *clones = NULL;
     uint8_t clones_cid[AUTH_UID_LEN];
+    time_t lastmod;
     rc_ty rc;
+
     if(has_arg("clones"))
         clones = get_arg("clones");
 
@@ -1285,7 +1287,13 @@ void fcgi_list_users(void) {
         else
             quit_errmsg(rc2http(rc), rc2str(rc));
     }
-    CGI_PUTS("Content-type: application/json\r\n\r\n{");
+    CGI_PUTS("Content-type: application/json\r\n");
+
+    lastmod = sx_users_lastmod(hashfs);
+    if(lastmod > 0 && is_object_fresh(NULL, 0, lastmod))
+	return;
+
+    CGI_PUTS("\r\n");
     rc = sx_hashfs_list_users(hashfs, clones ? clones_cid : NULL, print_user, has_arg("desc"), has_arg("quota"), has_arg("userMeta"), has_arg("customUserMeta"), &first);
     CGI_PUTS("}");
     if (rc != OK)

server/src/common/hashfs.c
@@ -8468,6 +8468,17 @@ rc_ty sx_hashfs_list_users(sx_hashfs_t *h, const uint8_t *list_clones, user_list
     return rc;
 }
 
+time_t sx_users_lastmod(sx_hashfs_t *h) {
+    time_t lastmod = (time_t)-1; /* Same as NO_LAST_MODIFIED */
+
+    sqlite3_reset(h->q_getval);
+    if(!qbind_text(h->q_getval, ":k", "user_lastmod") && qstep(h->q_getval) == SQLITE_ROW)
+	lastmod = sqlite3_column_int64(h->q_getval, 0);
+    sqlite3_reset(h->q_getval);
+
+    return lastmod;
+}
+
 /* Check if user with given uid is a volume owner */
 static int uid_is_volume_owner(sx_hashfs_t *h, const sx_hashfs_volume_t *vol, sx_uid_t id) {
     sqlite3_stmt *q = h->q_userisowner;

server/src/common/hashfs.h
@@ -29,6 +29,7 @@
 #define __HASHFS_H
 
 #include "default.h"
+#include <time.h>
 #include "utils.h"
 #include "nodes.h"
 #include "job_common.h"
@@ -207,6 +208,7 @@ typedef struct metacontent {
 rc_ty sx_hashfs_fill_usermeta_content(sx_hashfs_t *h, metacontent_t *meta, unsigned int *nmeta, sx_uid_t id);
 typedef int (*user_list_cb_t)(sx_hashfs_t *hashfs, sx_uid_t user_id, const char *username, const uint8_t *user, const uint8_t *key, int is_admin, const char *desc, int64_t quota, int64_t quota_usage, int print_meta, int print_custom_meta, int nmeta, metacontent_t *meta, void *ctx);
 rc_ty sx_hashfs_list_users(sx_hashfs_t *h, const uint8_t *list_clones, user_list_cb_t cb, int desc, int send_quota, int send_meta, int send_custom_meta, void *ctx);
+time_t sx_users_lastmod(sx_hashfs_t *h);
 /* Copy metadata entries stored in sx_blob_t into sxc_meta_t needed by sxi_proto_* functions from libsxclient. */
 int sx_hashfs_blob_to_sxc_meta(sxc_client_t *sx, sx_blob_t *b, sxc_meta_t **meta, int skip);
 

server/src/fcgi/fcgi-utils.c
@@ -865,7 +865,7 @@ int is_https(void) {
     return (proto && !strcasecmp(proto, "on"));
 }
 
-int is_object_fresh(const sx_hash_t *etag, char type, unsigned int last_modified) {
+int is_object_fresh(const sx_hash_t *etag, char type, time_t last_modified) {
     char tagbuf[3 + sizeof(*etag) * 2 + 1];
     const char *cond;
     int is_cached = 0, skip_modsince = 0;

server/src/fcgi/fcgi-utils.h
@@ -128,8 +128,8 @@ void send_keepalive(void);
 void send_nodes(const sx_nodelist_t *nodes);
 void send_nodes_randomised(const sx_nodelist_t *nodes);
 void send_job_info(job_t job);
-#define NO_LAST_MODIFIED 0xffffffff
-int is_object_fresh(const sx_hash_t *etag, char type, unsigned int last_modified);
+#define NO_LAST_MODIFIED ((time_t)(-1))
+int is_object_fresh(const sx_hash_t *etag, char type, time_t last_modified);
 
 #define quit_errmsg(errnum, message) do { send_error(errnum, message); return; } while(0)
 #define quit_errnum(errnum) do { send_error(errnum, NULL); return; } while(0)

commit b19786200e13cfc3e1f95006b72095a7b58d69fa

Fix typo introduced in 0113b9005563e922e1ea6ac1987bea8a8bfd8fac

server/src/fcgi/fcgi-actions-user.c
@@ -1293,7 +1293,7 @@ void fcgi_list_users(void) {
     if(lastmod > 0 && is_object_fresh(NULL, 0, lastmod))
 	return;
 
-    CGI_PUTS("\r\n");
+    CGI_PUTS("\r\n{");
     rc = sx_hashfs_list_users(hashfs, clones ? clones_cid : NULL, print_user, has_arg("desc"), has_arg("quota"), has_arg("userMeta"), has_arg("customUserMeta"), &first);
     CGI_PUTS("}");
     if (rc != OK)